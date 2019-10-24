9d4b3692add274f828a317bbbbf3a3dc3c29c290
Your Name <you@example.com>
2019-10-24 12:24:47 -0700

    Initial commit + complete printIndices

---
+++ arrays.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+/** 1. printIndices */
+const printIndices = (items) => {
+	// Replace this with your code
+  // for index in array
+  //    print array[index], index
+  for (const i in items) {
+    console.log(items[i], i);
+  }
+};
+
+
+/** 2. everyOtherItem */
+const everyOtherItem = (items) => {
+	// Replace this with your code
+};
+
+
+/** 3. smallestNItems */
+const smallestNItems = (items, n) => {
+	// Replace this with your code
+};
