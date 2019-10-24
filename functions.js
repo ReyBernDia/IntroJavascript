2b4c4b6f84f14c40bee191ba627adece7dee8c40
Your Name <you@example.com>
2019-10-24 16:16:22 -0700

    Completed functions.js

--- functions.js
+++ functions.js
@@ -16,9 +16,35 @@ console.log(isHometown('San Francisco'));
 
 /** 2. getFullName */
 
-// Define your function here
+const getFullName = (firstName, lastName) => {
 
+  return `${firstName} ${lastName}`;
+
+};
+
+console.log(getFullName('Brighticorn', 'Hackbright'));
 
 /** 3. calculateTotal */
 
-// Define your function here
+const calculateTotal = (basePrice, state, tax = 0.05) => {
+
+  const subtotal = basePrice * (1 + tax);
+
+  let fee = 0;
+  if (state === 'CA') {
+    fee = 0.03 * subtotal;
+  } else if (state === 'PA') {
+    fee = 2;
+  } else if (state === 'MA') {
+    if (basePrice <= 100) {
+      fee = 1;
+    } else {
+      fee = 3;
+    }
+  }
+  return subtotal + fee;
+};
+
+console.log(calculateTotal(2.00, 'CA'))
+console.log(calculateTotal(2.00, 'PA', 0.10))
+console.log(calculateTotal(105, 'MA'))
